bfdc3632163b246119bdfbe508218b1ec39a585c
Igor Evdokimov <i.evdokimov@g.nsu.ru>
2026-06-17 23:18:09 +0700

    railway connected

--- db.js
+++ db.js
@@ -1,16 +1,10 @@
 const { Pool } = require("pg");
-require("dotenv").config();
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
-
-pool.connect()
-  .then(() => console.log("DB OK"))
-  .catch(err => console.error("DB ERROR", err));
 
 module.exports = pool;
