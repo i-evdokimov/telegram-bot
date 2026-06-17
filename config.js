72c6fb1e5fc9d2dc6d5d191024e1a4b8a3c2c207
Igor Evdokimov <i.evdokimov@g.nsu.ru>
2026-06-18 01:54:23 +0700

    user id

--- config.js
+++ config.js
@@ -2,6 +2,6 @@ require("dotenv").config();
 
 module.exports = {
   BOT_TOKEN: process.env.BOT_TOKEN,
-  ADMIN_ID: wakemeuparalyzed,
+  ADMIN_ID: 1266151777,
   DATABASE_URL: process.env.DATABASE_URL,
 };
